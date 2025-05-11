985e73cfa6d9d185d1f0e47eca6f3a5bcf4cd8aa
Alexanderu9 <alexanderu7.aug@gmail.com>
2025-05-11 19:49:51 -0300

    Ex 1 – Cardápio Digital: Menu
    Nossa missão, agora, é a criação de um menu para uma página de cardápio digital. Usaremos a componentização do React para isso. Nosso menu deve ter o nome do restaurante (Mamamia Massas) e, as opções de menu:
    
    Início;
    
    Gnocchi;
    
    Pastas;
    
    Bedidas.

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import CardapioDigital from './componentes/CardapioDigital'
+import'./App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="container">
+<CardapioDigital />
+
+    </div>
+    </>
+  )
+}
+
+export default App
